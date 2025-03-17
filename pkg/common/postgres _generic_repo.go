680b60963c77bde36dee859a97d352cc5a1e186f
PinEye Marketing <mahdihp.devsc@gmail.com>
2025-03-17 14:14:17 +0330

    update UserRepository

---
+++ pkg/common/postgres _generic_repo.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"Amooz/database/postgres"
+)
+
+const (
+	SearchParamsType_Integer  string = "integer"
+	SearchParamsType_String   string = "string"
+	SearchParamsType_DateTime string = "datetime"
+)
+
+type SearchParams struct {
+	Type      string
+	Key       string
+	Value     string
+	SortField string
+}
+type IGenericRepository[T any] interface {
+	//Delete(filterParams SearchParams) error
+	//Update(filterParams SearchParams, updateDoc T) error
+	//Create(newItem T) error
+	//FindAll(params []SearchParams, pageIndex, pageSize int) ([]T, error)
+}
+type GenericRepository[T any] struct {
+	postgres *postgres.PostgresDb
+}
