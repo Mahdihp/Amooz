419b12d15c889465bde8ede3a358016c2c7f12b7
PinEye Marketing <mahdihp.devsc@gmail.com>
2025-03-17 11:54:18 +0330

    update UserRepository

--- internal/user/domain/user.go
+++ internal/user/domain/user.go
@@ -9,5 +9,6 @@ type User struct {
 	DisplayName string `json:"displayName,omitempty"`
 	Username    string `json:"username" gorm:"uniqueIndex;type:varchar(100);not null"`
 	Password    string `json:"password" gorm:"type:varchar(255);not null"`
+	Deleted     bool   `json:"deleted"`
 	Roles       []Role `json:"roles" gorm:"many2many:user_roles;"`
 }
--- internal/user/infrastructure/user_repository.go
+++ internal/user/infrastructure/user_repository.go
@@ -4,31 +4,67 @@ import (
 	"Amooz/database/postgres"
 	"Amooz/internal/user/domain"
 	"Amooz/pkg/config"
-	"context"
-	"errors"
+	"time"
 )
 
-// UserRepository مخزن کتاب‌ها است که داده‌ها را ذخیره می‌کند
+type IUserRepository interface {
+	Create(user domain.User) error
+	Update(user domain.User) error
+	Delete(user domain.User) error
+	SoftDelete(user domain.User) error
+	FindByID(userId int64) (domain.User, error)
+}
+
 type UserRepository struct {
 	postgres *postgres.PostgresDb
 }
 
-// NewUserRepository سازنده برای UserRepository
-func NewUserRepository(cfg config.Config) UserRepository {
+func NewUserRepository(cfg config.Config) IUserRepository {
 	//newPostgres := postgres.NewPostgres(cfg, []interface{}{&domain.User{}})
 
-	return UserRepository{
+	return &UserRepository{
 		postgres: nil,
 	}
 }
 
-// Save کتاب را ذخیره می‌کند
-func (r *UserRepository) Save(ctx context.Context, book *domain.User) error {
-	r.postgres.Db.Create(book)
-	return errors.New("not implemented")
+func (r *UserRepository) Update(user domain.User) error {
+	tx := r.postgres.Db.Model(&user).Updates(domain.User{DisplayName: user.DisplayName,
+		Password: user.Password, UpdatedAt: time.Now().UnixMilli()})
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
 }
 
-// FindByID کتابی را با شناسه پیدا می‌کند
-func (r *UserRepository) FindByID(ctx context.Context, id int64) (*domain.User, error) {
-	return nil, errors.New("not implemented")
+func (r *UserRepository) SoftDelete(user domain.User) error {
+	tx := r.postgres.Db.Model(&user).
+		Updates(domain.User{Deleted: true, UpdatedAt: time.Now().UnixMilli()})
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
+func (r *UserRepository) Delete(user domain.User) error {
+	tx := r.postgres.Db.Where("ID = ?", user.ID).Delete(&user)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
+
+func (r *UserRepository) Create(user domain.User) error {
+	tx := r.postgres.Db.Create(&user)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
+
+func (r *UserRepository) FindByID(userId int64) (domain.User, error) {
+	var user domain.User
+	tx := r.postgres.Db.First(&user, "ID = ?", userId)
+	if tx.Error != nil {
+		return domain.User{}, tx.Error
+	}
+	return user, nil
 }
